7b75b0bce44109ccc5ceb7a78e4d1581f3dccda2
Brothers On Tech <brothersontech@Brotherss-Mac-mini.local>
2024-03-12 21:27:42 -0400

    Updated Carousel.jsx with environment variables and self refresh functionality

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -2,17 +2,24 @@
 import React, { useState, useEffect } from "react";
 import { CarouselItem } from "./CarouselItem";
 import {createClient} from '@supabase/supabase-js'
-import { supabaseVars } from './SupabaseVariables';
+// import { supabaseVars } from './SupabaseVariables';
 
 
 export const Carousel = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [mangas, setManga] = useState([]);
-  const supabase = createClient(supabaseVars.url, supabaseVars.apiKey)
+  // const supabase = createClient(supabaseVars.url, supabaseVars.apiKey)
+  const supabase = createClient(process.env.REACT_APP_URL, process.env.REACT_APP_APIKEY)
 
+  // useEffect(() => {
+  //   getManga();
+  // }, []);
 
   useEffect(() => {
-    getManga();
+    const interval = setInterval(() => {
+      getManga();  
+    }, 5000);
+    return ()=> clearInterval(interval);
   }, []);
 
   async function getManga(){
@@ -22,26 +29,7 @@ export const Carousel = () => {
 
   }
 
-  const items = [
-    {
-      title: "Baseball",
-      description:
-        "Baseball is a bat-and-ball sport played between two teams of nine players each, taking turns batting and fielding. The game occurs over the course of several plays, with each play generally beginning when a player on the fielding team, called the pitcher.",
-      icon: require("./Media/example1.svg"),
-    },
-    {
-      title: "Walking",
-      description:
-        "Walking (also known as ambulation) is one of the main gaits of terrestrial locomotion among legged animals. Walking is typically slower than running and other gaits. ",
-      icon: require("./Media/example2.svg"),
-    },
-    {
-      title: "Weights",
-      description:
-        "Weightlifting generally refers to activities in which people lift weights, often in the form of dumbbells or barbells. People lift various kinds of weights for a variety of different reasons.",
-      icon: require("./Media/example3.svg"),
-    },
-  ];
+  
   const updateIndex = (newIndex) => {
     if (newIndex < 0) {
       newIndex = 0;
