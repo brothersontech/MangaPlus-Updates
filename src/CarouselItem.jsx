8310c7be93c670b6ae77ea9b376a00d41095c0fe
Brothers On Tech <brothersontech@Brotherss-Mac-mini.local>
2024-03-12 21:40:28 -0400

    Added Author

--- src/CarouselItem.jsx
+++ src/CarouselItem.jsx
@@ -17,6 +17,7 @@ export const CarouselItem = ({ manga, width }) => {
       <div></div>
       <a href={manga.url}><img className="carousel-img" src={manga.img_url} /></a>
       <div className="carousel-item-text"><a href={manga.url}>{manga.title}</a>
+      <p>Author: {manga.author}</p>
       <p>Description:</p>{manga.manga_description}</div>
     </div>
   );
